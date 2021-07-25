d6c973d302e30fcd6a2bff230a550273fcdffe94
Conary36 <crossingwaters@icloud.com>
2021-07-25 16:58:44 -0400

    mongoose added

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 const express = require('express');
 
 const app = express();
+
+//Middlewares
+app.use('/posts', ()=>{
+    console.log('middleware is running');
+});
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to Home page!');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('List of posts');
+});
+
+//Start listening to server
+app.listen(3000);
