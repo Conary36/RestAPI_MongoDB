56a2639a6408def575ec2bff159ea4fd3acb29b4
Conary36 <crossingwaters@icloud.com>
2021-07-25 23:07:16 -0400

    added routes and models/schema

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
+
+//Import routes
+const postRoute = require('./routes/post');
+
 //Middlewares
-app.use('/posts', ()=>{
-    console.log('middleware is running');
-});
+app.use('/posts', postRoute);
+
+// app.use('/posts', ()=>{
+//     console.log('middleware is running');
+// });
 
 
 //Routes
@@ -14,13 +21,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to Home page!');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('List of posts');
-});
+
 
 //Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, 
-    { useNewUrlParser: true }, ()=>{
+    { useNewUrlParser: true },{ useUnifiedTopology: true },()=>{
     console.log('connected to DB');
 });
 
