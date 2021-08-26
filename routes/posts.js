c87d929165d1ca857dc985b718bc2a1cae53c166
Conary36 <crossingwaters@icloud.com>
2021-08-25 23:40:02 -0400

    Update & Delete requests added

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
-
+//Get all posts
 router.get('/', async (req, res) => {
     // res.send('We are posting stuff');
     try {
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         console.log(message.err);
     }
 });
-
+//Get one post
 router.post('/', async (req, res) => {
     // console.log(req.body);
     const post = new Post({
@@ -34,16 +34,43 @@ router.post('/', async (req, res) => {
 
 });
 
+
+//Get one post by id
 router.get('/:postId', async (req, res) => {
     try{
     const post = await Post.findById(req.params.postId);
     res.json(post);
-    
+
     }catch(err){
         res.json(err.message);
     }
 
 });
 
+//Delete one post by id
+router.delete('/:postId', async (req, res) => {
+    try{
+    const removedPost = await Post.remove({_id: req.params.postId});
+    res.json(removedPost);
+
+    }catch(err){
+        res.json(err.message);
+    }
+
+
+});
+
+//Update one post by id
+router.patch('/:postId', async (req,res) => {
+    try{
+    const updatedPost = await Post.updateOne(
+        {_id: req.params.postId}, 
+        {$set: {title: req.body.title}},
+        {$set: {description: req.body.description}});
+    res.json(updatedPost);
+    }catch(err){
+        res.json(err.message);
+    }
+});
 
 module.exports = router;
